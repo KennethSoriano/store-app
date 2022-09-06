6a5710053d9792044dd92a122c31367c3685fbc5
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-06 14:05:15 -0700

    clear reducer

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,14 +1,14 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-import cartItems from "../cartItems";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
-    const { CartItem, total, amount} = useSelector((state) => state.cart)
+    const dispatch = useDispatch();
+    const { cartItems, total, amount} = useSelector((state) => state.cart)
   
     if (amount < 1) {
         return (
             <section className="cart">
-                {/* cart header */}
                 <header>
                     <h2>your bag</h2>
                     <h4 className="empty-cart">is currently empty</h4>
@@ -16,6 +16,7 @@ const CartContainer = () => {
             </section>
         );
     }
+
     return (
         <section className="cart">
             <header>
@@ -26,7 +27,6 @@ const CartContainer = () => {
                     return <CartItem key={item.id} {...item} />
                 })}
             </div>
-            {/* cart footer */}
             <footer>
                 <hr />
                 <div className="cart-total">
@@ -34,7 +34,7 @@ const CartContainer = () => {
                         total <span>${total}</span>
                     </h4>
                 </div>
-                <button className="btn clear-btn">clear cart</button>
+                <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
             </footer>
         </section>
     )
